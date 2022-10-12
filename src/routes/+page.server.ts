9a60502b317972338dcba4e233d11cf27eea23d8
Muhammad Yasser <roktmy@gmail.com>
2022-10-12 21:11:15 +0200

    updated api url string

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,8 @@
 import type { PageServerLoad } from "./$types";
 import type { PrayersData } from "$lib/models/models";
 
-const url = "http://localhost:8080/api/times";
+// const url = "http://localhost:8080/api/times";
+const url = "https://prayer-times-api.onrender.com/api/times";
 
 export const load: PageServerLoad = async ({ fetch }) => {
   const response = await fetch(url);
